d01a0f8817d86cfd6ce33ef202dff536700c1014
David Golden <xdg@xdg.me>
2017-04-15 09:59:14 -0400

    Silence Perl::Critic

--- lib/CPAN/Mini/Devel/Recent.pm
+++ lib/CPAN/Mini/Devel/Recent.pm
@@ -278,7 +278,7 @@ sub _parse_module_index {
     }
 
     my $mirror_list =
-        [ sort map { s{^(((.).).+)$}{authors/id/$3/$2/$1}; $_ } keys %mirror ];
+        [ sort map { s{^(((.).).+)$}{authors/id/$3/$2/$1}; $_ } keys %mirror ]; ## no critic
 
     return $mirror_list;
 }
